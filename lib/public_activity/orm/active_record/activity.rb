42f9f75761afa6e8ebce883eeabfbfb4b4c9b48a
Buffy Miller <buffy@sethmiller.com>
2015-09-21 10:45:20 -0400

    add with deleted to paranoia gem

--- lib/public_activity/orm/active_record/activity.rb
+++ lib/public_activity/orm/active_record/activity.rb
@@ -8,11 +8,11 @@ module PublicActivity
         self.table_name = PublicActivity.config.table_name
 
         # Define polymorphic association to the parent
-        belongs_to :trackable, :polymorphic => true
+        belongs_to :trackable, lambda { |record| record.respond_to?(:with_deleted) ? with_deleted : record }, :polymorphic => true
         # Define ownership to a resource responsible for this activity
-        belongs_to :owner, :polymorphic => true
+        belongs_to :owner, lambda { |record| record.respond_to?(:with_deleted) ? with_deleted : record }, :polymorphic => true
         # Define ownership to a resource targeted by this activity
-        belongs_to :recipient, :polymorphic => true
+        belongs_to :recipient, lambda { |record| record.respond_to?(:with_deleted) ? with_deleted : record }, :polymorphic => true
         # Serialize parameters Hash
         serialize :parameters, Hash
 
