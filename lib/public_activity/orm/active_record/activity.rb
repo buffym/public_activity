c69a577dfd89e1cf1c6419b43861ad2a7d054695
Buffy Miller <buffy@sethmiller.com>
2015-09-22 08:56:12 -0400

    add unscoped trackable to public activity

--- lib/public_activity/orm/active_record/activity.rb
+++ lib/public_activity/orm/active_record/activity.rb
@@ -8,17 +8,30 @@ module PublicActivity
         self.table_name = PublicActivity.config.table_name
 
         # Define polymorphic association to the parent
-        belongs_to :trackable, -> { unscoped }, :polymorphic => true
+        belongs_to :trackable, :polymorphic => true
         # Define ownership to a resource responsible for this activity
-        belongs_to :owner, -> { unscoped }, :polymorphic => true
+        belongs_to :owner, :polymorphic => true
         # Define ownership to a resource targeted by this activity
-        belongs_to :recipient, -> { unscoped }, :polymorphic => true
+        belongs_to :recipient, :polymorphic => true
         # Serialize parameters Hash
         serialize :parameters, Hash
 
         if ::ActiveRecord::VERSION::MAJOR < 4 || defined?(ProtectedAttributes)
           attr_accessible :key, :owner, :parameters, :recipient, :trackable
         end
+
+        def unscoped_trackable
+          unless @unscoped_trackable
+            klazz = self.trackable_type.constantize
+            if klazz.respond_to?(:with_deleted)
+              @unscoped_trackable = klazz.with_deleted.where(id: self.trackable_id).first
+            else
+              @unscoped_trackable = self.trackable
+            end
+          end
+          @unscoped_trackable
+        end
+
       end
     end
   end
